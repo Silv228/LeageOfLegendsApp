16ddf0620398bf6ffdc2bf79fff268fcb6a94bef
Silv228 <kosenkom672@gmail.com>
2023-08-11 03:34:10 +0300

    debug video player

--- csgotrades/app/(Champion)/Components/SpellVideo/SpellVideo.props.ts
+++
@@ -1,5 +0,0 @@
-import { DetailedHTMLProps, HtmlHTMLAttributes } from "react";
-
-export interface SpellVideoProps extends DetailedHTMLProps<HtmlHTMLAttributes<HTMLDivElement>, HTMLDivElement> {
-    url: string
-}
