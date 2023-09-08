674dd805afda66b9a7a1ec1b62dfb6ef1810e7cc
Silv228 <kosenkom672@gmail.com>
2023-09-08 11:27:21 +0300

    styling filter Component

--- app/components/Sort/Sort.tsx
+++ app/components/Sort/Sort.tsx
@@ -2,24 +2,31 @@
 import { translate } from "@/app/helpers/translate";
 import React, { ReactNode, useEffect, useState } from "react";
 import { SortProps } from "./Sort.props";
+import styles from "./Sort.module.css"
+import FilterIcon from "./filter-svgrepo-com.svg"
+import Image from "next/image";
 
-const Sort = ({ sortedData, sortedKeys, setData }: SortProps) => {
-    const [activeKey, setActiveKey] = useState('')
+const Sort = ({ resetValue, sortedData, sortedKeys, setData }: SortProps) => {
+    const [activeKey, setActiveKey] = useState(resetValue)
     const [keys, setKeys] = useState<ReactNode[]>([<></>])
+    const [showFilters, setShowFilters] = useState(false)
     useEffect(() => {
-        if (activeKey) {
+        if (activeKey !== resetValue) {
             setData(sortedData.filter((data) => data.tags.includes(activeKey)))
         }
-        else{
+        else {
             setData(sortedData)
         }
     }, [activeKey])
     useEffect(() => {
-        setKeys(sortedKeys.map((key: string) => <div key={key} onClick={() => setActiveKey(key)}>{translate(key)}</div>))
+        setKeys(sortedKeys.map((key: string) => <div className={styles.filterItem} key={key} onClick={() => setActiveKey(key)}>{translate(key)}</div>))
     }, [])
     return (
         <div>
-            {keys}
+            <Image onClick={() => setShowFilters(!showFilters)} className={styles.filterIcon} src={FilterIcon} alt="filter" width={32} height={32} />
+            <div className={!showFilters ? styles.hideFilters : styles.filters}>
+                {keys}
+            </div>
         </div>
     )
 }
